046151bff2693c38d4b4665d281d6281a3146662
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-03-26 17:19:26 +0530

    deployment fixed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,52 +1,52 @@
 import discount from "./Discount.svg";
-import community from "./Community.gif";
 import apple from "./apple.svg";
 import arrowUp from "./arrow-up.svg";
+import chat from "./chat.svg";
 import close from "./close.svg";
+import community from "./community.gif";
+import emergency from "./emergency.svg";
 import facebook from "./facebook.svg";
 import google from "./google.svg";
 import instagram from "./instagram.svg";
+import lastmockup from "./lastmockup.png";
 import linkedin from "./linkedin.svg";
+import magnify from "./magnify.svg";
 import menu from "./menu.svg";
+import money from "./money.svg";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
+import pin from "./pin.svg";
 import quotes from "./quotes.svg";
 import robot from "./robot.png";
 import logo from "./safeconnect.png";
-import twitter from "./twitter.svg";
-import tag from "./tag.svg";
-import money from "./money.svg";
-import magnify from "./magnify.svg";
 import scan from "./scan.svg";
-import chat from "./chat.svg";
-import pin from "./pin.svg";
-import emergency from "./emergency.svg";
-import lastmockup from "./lastmockup.png";
+import tag from "./tag.svg";
+import twitter from "./twitter.svg";
 export {
-  lastmockup,
-  emergency,
-  tag,
-  money,
-  magnify,
-  community,
-  scan,
-  chat,
-  pin,
   apple,
   arrowUp,
+  chat,
   close,
+  community,
   discount,
+  emergency,
   facebook,
   google,
   instagram,
+  lastmockup,
   linkedin,
   logo,
+  magnify,
   menu,
+  money,
   people01,
   people02,
   people03,
+  pin,
   quotes,
   robot,
+  scan,
+  tag,
   twitter,
 };
